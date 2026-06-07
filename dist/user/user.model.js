871dd2bcbc31d1c79f24f71d15bf96101920d046
Fathi Hindi <fhindi@orienteed.com>
2026-06-07 15:15:45 +0300

    feat: add UserModel class and associated build artifacts

--- dist/user/user.model.js
+++ dist/user/user.model.js
@@ -2,11 +2,12 @@ import { AddressModel } from "../address/address.model.js";
 import { UserProfileModel } from "./user-profile.model.js";
 export class UserModel {
     constructor(data) {
+        var _a;
         this.id = data.id;
         this.userName = data.userName;
         this.password = data.password;
         this.status = data.status;
-        this.role = data.role;
+        this.roles = ((_a = data.userRoles) === null || _a === void 0 ? void 0 : _a.map((ur) => ur.role)) || [];
         this.lastLogin = data.lastLogin;
         this.createdDate = data.createdDate;
         this.updatedDate = data.updatedDate;
